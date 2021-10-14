ca283196e56f0af6f21824549ae8729046bf88bd
82103 <82103@LAPTOP-NVK0ATAP>
2021-10-14 14:07:34 +0900

    분석중

---
+++ zombie/src/zombie/Main.java
@@ -0,0 +1,9 @@
+package zombie;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Game.getinstance().run();
+	}
+
+}
---
+++ zombie/src/zombie/Zombie.java
@@ -0,0 +1,8 @@
+package zombie;
+
+public class Zombie extends Unit{
+	public Zombie(String name,int hp,int att,int def,int pos) {
+		super(name, hp, att, def,pos);
+	}
+
+}
